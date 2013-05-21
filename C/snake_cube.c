e28b3b2ec02c940904fa88a82578ad449f40c81b
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-05-21 09:39:38 -0500

    decided to do linked list structure...

--- C/snake_cube.c
+++ C/snake_cube.c
@@ -1,53 +1,77 @@
+/* TODO:
+ * refractor block_g out as a global
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 
-typedef struct {
+typedef struct position_t {
   unsigned int x;
   unsigned int y;
   unsigned int z;
   int d;
-} position_t;
+  struct position_t *prev;
+}position_t;
 
 typedef struct {
   unsigned int x;
   unsigned int y;
   unsigned int z;
+  unsigned int total_blocks;
+  unsigned int blocks_traveled;
 } cube_t;
 
-cube_t *cube_g;
-
-
-
-bool position_valid( position_t **positions) {
+/*
+bool positions_equal( position_t * position_a , position_t * position_b) { 
   return true;
 }
+*/  
+/*
+bool position_valid( position_t **positions, posisition_t *position) {
+  
+  return false/*
+  int i;
+  for( i = 0; i < cube_g->blocks_traveled; i++) {
+    if( positions_equal( positions[i], positionl)) {
+      return false;
+    } }
+  return true;*/
+//}
 
-bool snake_valid( position_t **positions, char *binary_string);
-bool snake_valid( position_t **positions, char *binary_string) {
-  if( strlen( binary_string) == 1){
+bool snake_valid( cube_t *cube, position_t *position, char *binary_string);
+bool snake_valid( cube_t *cube, position_t *position, char *binary_string) {
+  if( strlen( binary_string) == 1) {
     printf( "%c!\n", binary_string[0] );
     return true;
   }
-  
-  if( binary_string[0] == '0'){
+
+  if( binary_string[0] == '0') {
     printf( "%c", binary_string[0] );
-    snake_valid( positions, binary_string + 1);
+    snake_valid( cube, position, binary_string + 1);
   }
   else {
     printf( "%c", binary_string[0] );
-    snake_valid( positions, binary_string + 1);
+    snake_valid( cube, position, binary_string + 1);
   }
     
 }
 
-void initialize( int argc, char *argv[]) {
-  cube_g = (cube_t *)malloc( sizeof( cube_t));
-  cube_g->x = (unsigned int)strtol(argv[1], (char **)NULL, 10);
-  cube_g->y = (unsigned int)strtol(argv[2], (char **)NULL, 10);
-  cube_g->z = (unsigned int)strtol(argv[3], (char **)NULL, 10);
+void initialize( int argc, char *argv[], cube_t *cube, position_t * position) {
+  cube = (cube_t *)malloc( sizeof( cube_t));
+  cube->x = (unsigned int)strtol(argv[1], (char **)NULL, 10);
+  cube->y = (unsigned int)strtol(argv[2], (char **)NULL, 10);
+  cube->z = (unsigned int)strtol(argv[3], (char **)NULL, 10);
+  cube->total_blocks = cube->x * cube->y * cube->z;
+  cube->blocks_traveled = 1;
+  position = (position_t *)malloc( sizeof( position_t));
+  position->x = 1;
+  position->y = 1;
+  position->z = 1;
+  position->d = 1;
+  position->prev = NULL;
 }
 
 void free_positions( position_t ** positions) {
@@ -57,16 +81,11 @@ void free_positions( position_t ** positions) {
 
 /* $ snake_cube x y z binary_string */
 int main (int argc, char *argv[]) {
-  initialize( argc, argv);
-  position_t **positions;
-  positions = (position_t **)malloc( sizeof( position_t*));
-  positions[0] = (position_t *)malloc( sizeof( position_t));
-  positions[0]->x = 1;
-  positions[0]->y = 1;
-  positions[0]->z = 1;
-  positions[0]->d = 1;
+  cube_t *cube;
+  position_t * position;
+  initialize( argc, argv, cube, position);
 
-snake_valid( positions, argv[4]);
+  snake_valid( cube, position, argv[4]);
 
   
   return 0;
